615471fcd45e55b1ed851e57a0b7beb26423bfeb
lucasalavapena <lucas.alavapena@gmail.com>
2017-01-26 16:07:37 +0000

    Added some comments

--- tb2_individual.c
+++ tb2_individual.c
@@ -2,32 +2,36 @@
 #include <math.h>
 
 int main () {
+	//declare variables and set their according data types
 	int no_streamlines, counter,counter2;
 	double x_old,u_old,y_old,v_old,dt,x_new,y_new,x_initial,x_bar,y_bar,u_bar,v_bar;
 	
-	
+	//open/create file and make it writeable
 	FILE*fid1;
 	fid1=fopen("stream.plt","w");
+	//write 
 	fprintf(fid1, "VARIABLES = \"x\" \"y\"");
 
 	printf (" Please enter the number of streamlines wanted for plotting:");
 	//add error message plz
 	scanf ("%i" ,&no_streamlines);
 	
+	//set variables to the required or self select value
 	x_old=-1;
 	dt=0.01;
 	counter=0;
 	counter2=0;
 	x_initial=-1;
 	
+	//loop for each different streamline
 	while (counter<no_streamlines)
 	{
 	counter=counter+1;	
-	fprintf(fid1, "\nZONE T=""\%i \"", counter);
+	fprintf(fid1, "\nZONE T=\%i ", counter);
 	y_old=-0.75+(1.5/(no_streamlines-1))*(counter-1);
 	fprintf(fid1, "\n%lf    %lf", x_initial, y_old);
 	
-	
+		//loop for each different streamline point while checking for conditions given in the excercise
 		while ((-1<=y_new<=1)&&-1<=x_new<=1)
 			{
 				/*u_old=1+(x_old)/(2*3.1415926*((x_old*x_old)+(y_old*y_old)));
